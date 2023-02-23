5677aba058e34a5ff6f089c56ed96a8acfa841f7
PauloSBL <114448029+PauloSBL@users.noreply.github.com>
2023-02-22 21:03:44 -0300

    Add files via upload

--- models/conn.js
+++ models/conn.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('pokedex', 'root','',{
-    host:'localhost',
+const sequelize = new Sequelize('bzkxobzzkbmyxa6gfrmy', 'umg39hwkyp1z1n0v','jQXFw0cQfN8gA8ZGeSSw',{
+    host:'bzkxobzzkbmyxa6gfrmy-mysql.services.clever-cloud.com',
     dialect: 'mysql',
 })
 
